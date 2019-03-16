df88a0a4d95769a60b6dadd54bdaf1e26fab1d44
Farjana Huq <fa.huq03@gmail.com>
2019-03-16 13:12:31 -0700

    Mongo Dependencies

---
+++ server.js
@@ -0,0 +1,113 @@
+// Using the tools and techniques you learned so far,
+// you will scrape a website of your choice, then place the data
+// in a MongoDB database. Be sure to make the database and collection
+// before running this exercise.
+
+// Consult the assignment files from earlier in class
+// if you need a refresher on Cheerio.
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+// TODO: make two more routes
+app.get("/scrapedData", function(req, res){
+
+  axios.get("https://www.cnn.com/opinions").then(function(response) {
+
+  // Load the HTML into cheerio
+  var $ = cheerio.load(response.data);
+
+  // Make an empty array for saving our scraped info
+  var results = [];
+  
+  // Route 1
+  // =======
+  // This route will retrieve all of the data
+  // from the scrapedData collection as a json (this will be populated
+  // by the data you scrape using the next route)
+  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
+      $("h3.cd__headline").each(function(i, element) {
+
+        var title = $(element).find("span.cd__headline-text").text();
+        var link = "https://www.cnn.com/opinions" + $(element).children("a").attr("href");
+        
+        
+        console.log("Title",title);
+        console.log("Link",link);
+        // If this found element had both a title and a link
+        if (title && link) {
+          // Insert the data in the scrapedData db
+          db.scrapedData.insert({
+            title: title,
+            link: link
+          },
+          function(err, inserted) {
+            if (err) {
+              // Log the error if one is encountered during the query
+              console.log(err);
+            }
+            else {
+              // Otherwise, log the inserted data
+              console.log(inserted);
+            }
+          });
+        }
+      });
+
+      // After looping through each element found, log the results to the console
+      console.log(results);
+    });
+      res.end();
+})
+
+
+// Route 2
+// =======
+// When you visit this route, the server will
+// scrape data from the site of your choice, and save it to
+// MongoDB.
+app.get("/all", function(req, res) {
+  // Query: In our database, go to the animals collection, then "find" everything
+  db.scrapedData.find({}, function(err, data) {
+    // Log any errors if the server encounters one
+    if (err) {
+      console.log(err);
+    }
+    else {
+      // Otherwise, send the result of this query to the browser
+      res.json(data);
+    }
+  });
+});
+// TIP: Think back to how you pushed website data
+// into an empty array in the last class. How do you
+// push it into a MongoDB collection instead?
+
+/* -/-/-/-/-/-/-/-/-/-/-/-/- */
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
